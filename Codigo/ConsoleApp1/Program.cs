ce09f14954d5282286ca668e8308113726aa53b3
Jeisson <Jeissonfarfan712@gmail.com>
2024-11-29 20:06:35 -0500

    Se genera Ingreso del Codigo

--- Codigo/ConsoleApp1/Program.cs
+++ Codigo/ConsoleApp1/Program.cs
@@ -10,6 +10,18 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+                Console.WriteLine("Ingresa un número:");
+                int num = int.Parse(Console.ReadLine());
+
+                if (num % 2 == 0)
+                {
+                    Console.WriteLine("El número {0} es par.", num);
+                }
+                else
+                {
+                    Console.WriteLine("El número {0} es impar.", num);
+                }
+                Console.ReadKey();
+            }
         }
     }
-}
